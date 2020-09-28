27862e908f4f7a0a06cbbad361b8c59d2c080172
seanstephens967 <seanstephens967@gmail.com>
2020-09-28 12:03:02 -0400

    Add project files.

---
+++ PairProgramming_ConsoleGame/Score.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairProgramming_ConsoleGame
+{
+    public class Score
+    {
+        public int GameScore { get; set; }
+        public Score()
+        {
+            GameScore = 20;
+        }
+
+        public int GetScore()
+        {
+            return GameScore;
+        }
+
+        public void CorrectAnswer()
+        {
+            GameScore += 10;
+        }
+
+        public void IncorrectAnswer()
+        {
+            GameScore -= 10;
+        }
+
+        public void PrintScore()
+        {
+            Console.WriteLine($"Your score: {GameScore}");
+        }
+    }
+}
